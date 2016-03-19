bb1961a0a9410f71bd9cbbc2a8e4c2923a6027bc
ashwin <m.ashwinkumar7@gmail.com>
2016-03-19 10:20:27 -0400

    Project Calcultor

---
+++ calculator.js
@@ -0,0 +1,54 @@
+/* Calculator.js */
+
+function calculateTip() {
+    
+    // Store the data of inputs
+    var billAmount = document.getElementById("billAmount").value;
+    var serviceQuality = document.getElementById("serviceQuality").value;
+    var numPeople = document.getElementById("totalPeople").value;
+    
+    // Quick validation
+    if(billAmount === "" || serviceQuality == 0) {
+        window.alert("Please enter some values to get this baby up and running!");
+        return; // this will prevent the function from continuing
+    }
+    
+    // Check to see if this input is empty or less than or equal to 1
+    if(numPeople === "" || numPeople <= 1) {
+        numPeople = 1;
+        
+        document.getElementById("each").style.display = "none";
+        
+    } else {
+        
+        document.getElementById("each").style.display = "block";
+        
+    }
+    
+    // Do some math!
+    var total = (billAmount * serviceQuality) / numPeople;
+	window.alert(total);
+   // total = Math.round(total * 100) / 100;
+    // total = total.toFixed(2);
+    
+    
+    // Display the tip!
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+    
+}
+
+// Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+// Clicking the button calls our custom function
+document.getElementById("calculate").onclick = function() { calculateTip(); };
+
+
+
+
+
+
+
+
